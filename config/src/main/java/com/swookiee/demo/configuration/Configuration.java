8b5496ab554279f7daa7c8e1a351a7bc9f8c8d09
Lars Pfannenschmidt <lars@mobilecologne.de>
2014-06-12 12:31:28 -0400

    added configuration for username/pasword

--- config/src/main/java/com/swookiee/demo/configuration/Configuration.java
+++ config/src/main/java/com/swookiee/demo/configuration/Configuration.java
@@ -1,10 +1,22 @@
 package com.swookiee.demo.configuration;
 
+import com.swookiee.runtime.authentication.AdminUserConfiguration;
 import com.swookiee.runtime.metrics.configuration.GraphiteReporterConfiguration;
 
 /*
  * This is a simple POJO which will be filled from the Configuration.yaml from src/main/resources
  */
 public class Configuration {
+
+    /**
+     * We want to read Graphite Configuration from the Configuration.yaml
+     */
     public GraphiteReporterConfiguration graphiteReporterConfiguration;
+
+    /**
+     * We also want to change the default username and pasword for the felix
+     * console and the remote API
+     */
+    public AdminUserConfiguration adminUserConfiguration;
+
 }
